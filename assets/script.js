9c03a5e02b4e90c6b2a9523322156e6a9f68c488
Andrea Iglio <andrea.iglio@gmail.com>
2021-01-27 00:18:25 +0100

    Implemented main with its first 2 sections using VueJS

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,51 @@
 let root = new Vue({
-
-    el: "#root",
-    data: {
-
-        logo_google: './assets/img/logo/google-logo.svg',
-        logo_slack: './assets/img/logo/slack-logo.svg',
-
-    },
-    methods: {
-
-    },
-
-
-
-})
+  el: "#root",
+  data: {
+    logo_google: "./assets/img/logo/google-logo.svg",
+    logo_slack: "./assets/img/logo/slack-logo.svg",
+    main_first_section: [
+      {
+        title:
+          "Traditional people processes lead to disengagement and turnover",
+        jumbo_pic: "./assets/img/main/section_1/before.png",
+        cards: [
+          {
+            img: "./assets/img/main/section_1/hardcore.png",
+            text: "lorem",
+          },
+          {
+            img: "./assets/img/main/section_1/nofeedback.png",
+            text: "lorem",
+          },
+          {
+            img: "./assets/img/main/section_1/notsafe.png",
+            text: "lorem",
+          },
+        ],
+      },
+    ],
+    main_second_section: [
+      {
+        title:
+          "It doesn't have to be like that. Teambit is a modern solution that teammates love.",
+        jumbo_pic: "./assets/img/main/section_2/after.png",
+        cards: [
+          {
+            img: "./assets/img/main/section_2/lightweight.png",
+            text: "lorem",
+          },
+          {
+            img: "./assets/img/main/section_2/feedbacksuggestions.png",
+            text: "lorem",
+          },
+          {
+            img: "./assets/img/main/section_2/safe.png",
+            text: "lorem",
+          },
+        ],
+      },
+      {},
+    ],
+  },
+  methods: {},
+});
